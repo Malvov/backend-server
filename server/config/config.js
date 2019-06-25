7340924df2e7510b3e306da7593d7dd4d3f998c5
malvov <manuel.olivaresbonilla@gmail.com>
2019-06-24 21:34:50 -0600

    deploying to heroku

--- server/config/config.js
+++ server/config/config.js
@@ -4,3 +4,23 @@
 
 process.env.PORT = process.env.PORT || 3000;
 
+
+//================
+// Environment
+//================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+//================
+// Connection string
+//================
+
+let connectionString;
+
+if (process.env.NODE_ENV === 'development') {
+    connectionString = 'mongodb://localhost:27017/cafe';
+} else {
+    connectionString = process.env.MONGO_CONNECTION_STRING;
+}
+
+process.env.CONNECTION_STRING = connectionString;
